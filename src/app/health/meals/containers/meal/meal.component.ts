480d2662b17a6a229e8f3fd21ffdb3d721c5e902
Anthony Fertil <a.fertil@of2m.fr>
2017-11-28 10:47:27 +0100

    Initialiaze the reactive form meal to be able to create a new meal with ingredients

--- src/app/health/meals/containers/meal/meal.component.ts
+++ src/app/health/meals/containers/meal/meal.component.ts
@@ -1,14 +1,30 @@
 import { Component } from '@angular/core';
 
+import { Meal } from './../../../shared/services/meals/meals.service';
+
 @Component({
   selector: 'app-meal',
   styleUrls: ['meal.component.scss'],
   template: `
     <div class="meal">
-      I am a meal!
+      <div class="meal__title">
+        <h1>
+          <img src="/assets/food.svg">
+          <span>Create meal</span>
+        </h1>
+      </div>
+      <div>
+        <app-meal-form
+          (create)="addMeal($event)">
+        </app-meal-form>
+      </div>
     </div>
   `
 })
 export class MealComponent {
   constructor() {}
+
+  addMeal(event: Meal) {
+    console.log('Meal:', event);
+  }
 }
--- src/app/health/meals/meals.module.ts
+++ src/app/health/meals/meals.module.ts
@@ -5,6 +5,9 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { SharedModule } from './../shared/shared.module';
 
+// Components
+import { MealFormComponent } from './components/meal-form/meal-form.component';
+
 // Containers
 import { MealsComponent } from './containers/meals/meals.component';
 import { MealComponent } from './containers/meal/meal.component';
@@ -23,7 +26,8 @@ export const ROUTES: Routes = [
   ],
   declarations: [
     MealsComponent,
-    MealComponent
+    MealComponent,
+    MealFormComponent
   ],
   providers: []
 })
