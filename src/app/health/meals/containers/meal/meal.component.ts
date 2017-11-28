7b877901e8770a747580ce52287e15f61c49ae7a
Anthony Fertil <a.fertil@of2m.fr>
2017-11-28 10:14:15 +0100

    Create the meal component to be able to create a new meal

---
+++ src/app/health/meals/containers/meal/meal.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-meal',
+  styleUrls: ['meal.component.scss'],
+  template: `
+    <div class="meal">
+      I am a meal!
+    </div>
+  `
+})
+export class MealComponent {
+  constructor() {}
+}
--- src/app/health/meals/containers/meals/meals.component.ts
+++ src/app/health/meals/containers/meals/meals.component.ts
@@ -10,8 +10,34 @@ import { MealsService, Meal } from '../../../shared/services/meals/meals.service
   selector: 'app-meals',
   styleUrls: ['meals.component.scss'],
   template: `
-    <div>
-      {{ meals$ | async | json }}
+    <div class="meals">
+      <div class="meals__title">
+        <h1>
+          <img src="/assets/food.svg">
+          Your meals
+        </h1>
+        <a
+          class="btn__add"
+          [routerLink]="['../meals/new']">
+          <img src="assets/add-white.svg">
+          New meal
+        </a>
+      </div>
+
+      <div *ngIf="meals$ | async as meals; else loading;">
+        <div class="message" *ngIf="!meals.length">
+          <img src="assets/face.svg">
+          No meals, add a new meal to start
+        </div>
+        <!-- meals ngFor -->
+      </div>
+
+      <ng-template #loading>
+        <div class="message">
+          <img src="assets/loading.svg">
+          Fetching meals...
+        </div>
+      </ng-template>
     </div>
   `
 })
